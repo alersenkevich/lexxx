a82202ab7336c9351c582bf611e3745e7e9a359c
alersenkevich <aler.senkevich3301@gmail.com>
2018-11-12 02:35:40 +0500

    augmented binance socket handler

--- src/lib/api/websocket/abstract-socket-event-wrapper.ts
+++ src/lib/api/websocket/abstract-socket-event-wrapper.ts
@@ -3,13 +3,12 @@ import { EventEmitter } from 'events';
 
 export abstract class AbstractSocketEventWrapper extends EventEmitter {
   protected socket: WebSocket = null;
+  public abstract initSocket(): void;
 
   constructor() {
     super();
     this.on('newListener', (event, listener) => this.initSocket());
   }
-
-  public abstract initSocket(): void;
 
   public reconnectSocket(error?: any): void {
     console.log(error);
--- src/lib/api/websocket/binance/index.ts
+++ src/lib/api/websocket/binance/index.ts
@@ -28,6 +28,16 @@ const converterValues = {
     a: 'sellerOrderId',
     m: 'isMarketMaker',
   },
+  '24hrMiniTicker': {
+    l: 'lowPrice',
+    e: 'eventType',
+    E: 'eventTime',
+    o: 'openPrice',
+    h: 'highPrice',
+    c: 'currentDayClosePrice',
+    v: 'totalTradedBaseAssetVolume',
+    q: 'totalTradedQuoteAssetVolume',
+  },
   '24hrTicker': {
     e: 'eventType',
     E: 'eventTime',
@@ -42,9 +52,9 @@ const converterValues = {
     B: 'bestBidQuantity',
     a: 'bestAskPrice',
     A: 'bestAskQuantity',
-    o: 'openPrice',
-    h: 'highPrice',
-    l: 'lowPrice',
+    o: 'open',
+    h: 'high',
+    l: 'low',
     v: 'totalTradedBaseAssetVolume',
     q: 'totalTradedQuoteAssetVolume',
     O: 'statisticsOpenTime',
@@ -53,98 +63,124 @@ const converterValues = {
     L: 'lastTradeId',
     n: 'totalNumberOfTrades',
   },
+  kline: {
+    stream: 'stream',
+    e: 'eventType',
+    E: 'eventTime',
+    s: 'symbol',
+    k: {
+      parentPropName: 'kline',
+      t: 'klineStartTime',
+      T: 'klineCloseTime',
+      s: 'symbol',
+      i: 'interval',
+      f: 'firstTradeId',
+      L: 'lastTradeId',
+      o: 'open',
+      c: 'close',
+      h: 'high',
+      l: 'low',
+      v: 'baseAssetVolume',
+      n: 'numberOfTrades',
+      x: 'isKlineClosed',
+      q: 'quoteAssetVolume',
+      V: 'takerBuyBaseAssetVolume',
+      Q: 'takerBuyQuoteAssetVolume',
+      B: 'ignore',
+    },
+  },
 };
 
 export interface IAggregatedSymbolTrades {
-  e: 'aggTrade';    // Event type
-  E: number;        // Event time
-  s: string;        // Symbol
-  a: number;        // Aggregate trade ID
-  p: string;        // Price
-  q: string;        // Quantity
-  f: number;        // First trade ID
-  l: number;        // Last trade ID
-  T: number;        // Trade time
-  m: boolean;       // Is the buyer the market maker?
-  M: boolean;       // Ignore
+  eventType:    'aggTrade';
+  eventTime:        number;
+  symbol:           string;
+  aggregateTradeId: number;
+  price:            string;
+  quantity:         string;
+  firstTradeId:     number;
+  lastTradeId:      number;
+  tradeTime:        number;
+  isMarketMaker:   boolean;
+  ignore:          boolean;
 }
 
 export interface ITrade {
-  e: 'trade';       // Event type
-  E: number;        // Event time
-  s: string;        // Symbol
-  t: number;        // Trade ID
-  p: string;        // Price
-  q: string;        // Quantity
-  b: number;        // Buyer order ID
-  a: number;        // Seller order ID
-  T: number;        // Trade time
-  m: boolean;       // Is the buyer the market maker?
-  M: boolean;       // Ignore
+  eventType:       'trade';
+  eventTime:        number;
+  symbol:           string;
+  tradeId:          number;
+  price:            string;
+  quantity:         string;
+  buyerOrderId:     number;
+  sellerOrderId:    number;
+  tradeTime:        number;
+  isMarketMaker:   boolean;
+  ignore:          boolean;
 }
 
 export interface ITicker {
-  e: '24hrTicker';  // Event type
-  E: number;        // Event time
-  s: string;        // Symbol
-  p: string;        // Price change
-  P: string;        // Price change percent
-  w: string;        // Weighted average price
-  x: string;        // Previous day's close price
-  c: string;        // Current day's close price
-  Q: string;        // Close trade's quantity
-  b: string;        // Best bid price
-  B: string;        // Best bid quantity
-  a: string;        // Best ask price
-  A: string;        // Best ask quantity
-  o: string;        // Open price
-  h: string;        // High price
-  l: string;        // Low price
-  v: string;        // Total traded base asset volume
-  q: string;        // Total traded quote asset volume
-  O: number;        // Statistics open time
-  C: number;        // Statistics close time
-  F: number;        // First trade ID
-  L: number;        // Last trade Id
-  n: number;        // Total number of trades
+  eventType:            '24hrTicker';
+  eventTime:                  number;
+  symbol:                     string;
+  priceChange:                string;
+  priceChangePercent:         string;
+  weightedAveragePrice:       string;
+  previousDayClosePrice:      string;
+  currentDayClosePrice:       string;
+  closeTradeQuantity:         string;
+  bestBidPrice:               string;
+  bestBidQuantity:            string;
+  bestAskPrice:               string;
+  bestAskQuantity:            string;
+  open:                       string;
+  high:                       string;
+  low:                        string;
+  totalTradedBaseAssetVolume: string;
+  totalTradedQuoteAssetVolume:string;
+  statisticsOpenTime:         number;
+  statisticsCloseTime:        number;
+  firstTradeId:               number;
+  lastTradeId:                number;
+  totalNumberOfTrades:        number;
 }
 
 export interface IKline {
-  stream: string;
-  e: 'kline';       // Event type
-  E: number;        // Symbol
-  s: string;        // Symbol
-  k: {
-    t: number;      // Kline start time
-    T: number;      // Kline close time
-    s: string;      // Symbol
-    i: string;      // Interval
-    f: number;      // First trade ID
-    L: number;      // Last trade ID
-    o: string;      // Open price
-    c: string;      // Close price
-    h: string;      // High price
-    l: string;      // Low price
-    v: string;      // Base asset volume
-    n: number;      // Number of trades
-    x: boolean;     // Is this kline closed?
-    q: string;      // Quote asset volume
-    V: string;      // Taker buy base asset volume
-    Q: string;      // Taker buy quote asset volume
-    B: string;      // Ignore
+  stream:             string;
+  eventType:         'kline';
+  eventTime:          number;
+  symbol:             string;
+  kline: {
+    klineStartTime:           number;
+    klineCloseTime:           number;
+    symbol:                   string;
+    interval:                 string;
+    firstTradeId:             number;
+    lastTradeId:              number;
+    open:                     string;
+    close:                    string;
+    high:                     string;
+    low:                      string;
+    baseAssetVolume:          string;
+    numberOfTrades:           number;
+    isKlineClosed:           boolean;
+    quoteAssetVolume:         string;
+    takerBuyBaseAssetVolume:  string;
+    takerBuyQuoteAssetVolume: string;
+    ignore:                   string;
   };
 }
 
 export interface IMiniTicker {
-  e: '24hrMiniTicker';  // Event type
-  E: number;            // Event time
-  s: string;            // Symbol
-  c: string;            // Current day's close price
-  o: string;            // Open price
-  h: string;            // High price
-  l: string;            // Low price
-  v: string;            // Total traded base asset volume
-  q: string;            // Total traded quote asset volume
+  eventType:          '24hrMiniTicker';
+  eventTime:                    number;
+  symbol:                       string;
+  currentDayClosePrice:         string;
+  open:                         string;
+  high:                         string;
+  low:                          string;
+  totalTradedBaseAssetVolume:   string;
+  totalTradedQuoteAssetVolume:  string;
 }
 
 export interface ISocketMessage {
